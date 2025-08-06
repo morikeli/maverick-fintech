3b582c702f9ebe6727b629586333f203f504ebb1
magnus.coder <kelimbth@gmail.com>
2025-08-06 11:19:29 +0300

    feat: added color and color scheme for the app

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF004D40);
+const Color kSecondaryColor = Color(0xFF9E9E9E);
+const Color kScaffoldBgLightColor = Color(0xFFFFFFFF);
+const Color kScaffoldBgDarkcolor = Color(0xFF000000);
+const Color kContainerLightColor = Color(0xCCE0E0E0);
+const Color kContainerDarkColor = Color(0xFF004D40);
+const Color kButtonDarkColor = Color(0xFF004D40);
+const Color kIconDarkColor = Color(0xFF004D40);
+const Color kIconLightColor = Color(0xFFFFFFFF);
+const Color kIconDangerColor = Color(0xFFCC180B);
+const Color kTextLightColor = Color(0xFFFFFFFF);
+const Color kTextGreenColor = Color(0xFF004D40);
+const Color kTextDarkColor = Color(0xFF000000);
+const Color kTextSecondaryColor = Color(0xFF8B8B8B);
+const Color kTextFormFieldLabelColor = Color(0xFF009688);
+const Color kDangerColor = Color(0xFFC21407);
