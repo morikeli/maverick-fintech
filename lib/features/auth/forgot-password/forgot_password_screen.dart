e85c0cf97b03db7aaad6187b952648c8af2bfa5c
magnus.coder <kelimbth@gmail.com>
2025-08-06 13:05:11 +0300

    feat: created screen to request for password reset

---
+++ lib/features/auth/forgot-password/forgot_password_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'components/forgot_password_body.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  static String routeName = '/forgot-password';
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ForgotPasswordBody(),
+    );
+  }
+}
