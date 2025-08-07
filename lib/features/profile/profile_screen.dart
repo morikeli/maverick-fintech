673e61ffae1148b1fba2dc439d4fdc50845194d9
magnus.coder <kelimbth@gmail.com>
2025-08-07 14:43:16 +0300

    feat: created profile screen

---
+++ lib/features/profile/profile_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:maverick_app/widgets/appbar.dart';
+
+import '../../controllers/theme_controller.dart';
+import 'components/profile_screen_body.dart';
+
+class ProfileScreen extends StatelessWidget {
+  static String routeName = '/profile';
+  ProfileScreen({super.key});
+
+  final ThemeController themeController = Get.find<ThemeController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: sharedAppBar(context, 'Profile'),
+      body: ProfileScreenBody(themeController: themeController),
+    );
+  }
+}
