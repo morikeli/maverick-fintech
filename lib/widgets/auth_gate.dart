880b0a153ae14aa6100dfcea46347504a269628d
magnus.coder <kelimbth@gmail.com>
2025-08-08 14:11:42 +0300

    feat: created a auth gate to redirect user to suitable screen

---
+++ lib/widgets/auth_gate.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../features/auth/login/login_screen.dart';
+import 'homescreen.dart';
+import 'loading_widget.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, asyncSnapshot) {
+        if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+          return LoadingWidget.newtonCradleMedium();
+        }
+        // if the user is still logged in, navigate to homescreen
+        if (asyncSnapshot.data != null) {
+          return HomeScreen();
+        }
+        return LoginScreen();
+      }
+    );
+  }
+}
