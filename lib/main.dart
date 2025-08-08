8b2c9e24e4c2ed3b01b22cc48e728987af5b9c7e
magnus.coder <kelimbth@gmail.com>
2025-08-08 13:49:39 +0300

    feat: wrapped GetMaterial with a ToastificationWrapper to display toasts in the app

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,19 @@ class MaverickApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Maverick',
-      darkTheme: MaverickAppTheme.darkTheme,
-      theme: MaverickAppTheme.lightTheme,
-      home: LoginScreen(),
-      initialRoute: '/onboarding-screen',
-      routes: routes,
-    );
+    return Obx(() {
+      return ToastificationWrapper(
+        child: GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Maverick',
+          darkTheme: MaverickAppTheme.darkTheme,
+          theme: MaverickAppTheme.lightTheme,
+          themeMode: themeController.themeMode.value,
+          home: HomeScreen(),
+          initialRoute: '/onboarding-screen',
+          routes: routes,
+        ),
+      );
+    });
   }
 }
