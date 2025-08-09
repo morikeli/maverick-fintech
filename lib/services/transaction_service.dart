f88908c7006c230876db68e0e04a5f9c16e1962a
magnus.coder <kelimbth@gmail.com>
2025-08-09 16:49:29 +0300

    feat: used `rxdart` package to match queries in `getTransactions()`

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:rxdart/rxdart.dart';
 import '../models/transaction_model.dart';
 
 class TransactionService {
