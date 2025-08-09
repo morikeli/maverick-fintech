e21ab52981febd17d3baed62f176c9c457ff7b76
magnus.coder <kelimbth@gmail.com>
2025-08-09 11:33:33 +0300

    refactor: deleted controller and updated import stmts

--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:maverick_app/widgets/appbar.dart';
 
-import '../../controllers/theme_controller.dart';
+import 'components/edit_profile_fab.dart';
 import 'components/profile_screen_body.dart';
 
 class ProfileScreen extends StatelessWidget {
   static String routeName = '/profile';
-  ProfileScreen({super.key});
-
-  final ThemeController themeController = Get.find<ThemeController>();
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
