d87391f5766dd4fe0f1a6c622d27e49a80184aa5
magnus.coder <kelimbth@gmail.com>
2025-08-10 17:27:32 +0300

    fix: when users log in redirect them to the PIN prompt screen

--- lib/features/auth/login/widgets/login_form.dart
+++ lib/features/auth/login/widgets/login_form.dart
@@ -56,7 +56,7 @@ class _LoginFormState extends State<LoginForm> {
               form.save();
               emailController.clear();
               passwordController.clear();
-              Get.offAndToNamed('/home');
+              Get.offAndToNamed('/pin-screen');
             } else {
               return AppToastsWidget.dangerToastification(
                 context,
