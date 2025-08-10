d2b97ac8eeb15cd979a0ba9c82dfe7b1476cd392
magnus.coder <kelimbth@gmail.com>
2025-08-10 18:46:03 +0300

    feat: created new const variables for profile pictures

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -4,3 +4,7 @@ const kAppName = 'Maverick';
 const kAppVersion = '1.0.0';
 const kAppDescription = 'Your gateway to seamless payments across the world.';
 
+const kDefaultProfilePic = 'assets/imgs/dps/default.png';
+const kMaleProfilePicture = 'assets/imgs/dps/5.jpg';
+const kFemaleProfilePicture = 'assets/imgs/dps/8.jpg';
+
