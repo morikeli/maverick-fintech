571284c33a75943911ffa4726374220553a274d3
magnus.coder <kelimbth@gmail.com>
2025-08-10 17:30:15 +0300

    feat: added routes for the pin setup, and reset screens

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,6 +3,9 @@ import '../widgets/homescreen.dart';
 
 import 'features/auth/forgot-password/forgot_password_screen.dart';
 import 'features/auth/login/login_screen.dart';
+import 'features/auth/pin/pin_screen.dart';
+import 'features/auth/pin/pin_setup_screen.dart';
+import 'features/auth/pin/reset_pin.dart';
 import 'features/onboarding/onboarding_screen.dart';
 import 'features/auth/otp/otp_screen.dart';
 import 'features/auth/reset-password/reset_password_screen.dart';
@@ -22,4 +25,7 @@ final Map<String, WidgetBuilder> routes = {
   ProfileScreen.routeName: (context) => ProfileScreen(),
   EditProfileScreen.routeName: (context) => EditProfileScreen(),
   TransactionScreen.routeName: (context) => TransactionScreen(),
+  PinSetupScreen.routeName: (context) => PinSetupScreen(),
+  ResetPinScreen.routeName: (context) => ResetPinScreen(),
+  PINScreen.routeName: (context) => PINScreen(),
 };
